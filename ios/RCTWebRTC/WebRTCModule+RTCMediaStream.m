f6d3e521201976b29464a9806fa7bccb1b89d102
Saúl Ibarra Corretgé <s@saghul.net>
2025-10-07 17:00:30 +0200

    ios: fix crash in iOS 26 simulator

--- ios/RCTWebRTC/WebRTCModule+RTCMediaStream.m
+++ ios/RCTWebRTC/WebRTCModule+RTCMediaStream.m
@@ -326,6 +326,9 @@ RCT_EXPORT_METHOD(enumerateDevices : (RCTResponseSenderBlock)callback) {
                                                                mediaType:AVMediaTypeVideo
                                                                 position:AVCaptureDevicePositionUnspecified];
     for (AVCaptureDevice *device in videoDevicesSession.devices) {
+        if (device.uniqueID == nil) {
+            continue;
+        }
         NSString *position = @"unknown";
         if (device.position == AVCaptureDevicePositionBack) {
             position = @"environment";
@@ -345,11 +348,15 @@ RCT_EXPORT_METHOD(enumerateDevices : (RCTResponseSenderBlock)callback) {
             @"kind" : @"videoinput",
         }];
     }
+
     AVCaptureDeviceDiscoverySession *audioDevicesSession =
         [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[ AVCaptureDeviceTypeBuiltInMicrophone ]
                                                                mediaType:AVMediaTypeAudio
                                                                 position:AVCaptureDevicePositionUnspecified];
     for (AVCaptureDevice *device in audioDevicesSession.devices) {
+        if (device.uniqueID == nil) {
+            continue;
+        }
         NSString *label = @"Unknown audio device";
         if (device.localizedName != nil) {
             label = device.localizedName;
